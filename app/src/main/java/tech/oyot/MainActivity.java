d071ec20adde26e700071df4f6e3fd01aad3d928
Jean Catulle <jjcatulle389@gmail.com>
2019-03-03 15:57:58 -0500

    I hope this works

--- app/src/main/java/tech/oyot/MainActivity.java
+++ app/src/main/java/tech/oyot/MainActivity.java
@@ -1,13 +1,131 @@
 package tech.oyot;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+    private DrawerLayout drawer;
+
+    ///logout//
+    private FirebaseAuth mAuth;
+    private TextView navName;
+    private TextView navEmail;
+    private FloatingActionButton newPost;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth=FirebaseAuth.getInstance();
+
+        navName=findViewById(R.id.nav_name);
+        navEmail=findViewById(R.id.nav_email);
+        newPost=findViewById(R.id.new_post);
+
+        //toolbar
+        Toolbar toolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer=findViewById(R.id.drawer_layout);
+        NavigationView navigationView =findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle= new ActionBarDrawerToggle(this,drawer,toolbar,
+                R.string.navigation_draw_open,R.string.navigation_draw_close);
+
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        newPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                postAct();
+            }
+        });
+
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()){
+            case R.id.nav_logout:
+                logOut();
+                break;
+            case R.id.nav_profile:
+                profile();
+                break;
+            /////make more case statements for functions of the nav header
+        }
+        return true;
+    }
+
+    private void postAct() {
+        Intent setupIntent = new Intent(MainActivity.this, PostActivity.class);
+        startActivity(setupIntent);
+        finish();
+    }
+
+    private void profile() {
+        Intent setupIntent = new Intent(MainActivity.this, SetupActivity.class);
+        startActivity(setupIntent);
+        finish();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        }else {
+            super.onBackPressed();
+
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseUser currentUser= FirebaseAuth.getInstance().getCurrentUser();
+///current user is not login
+        if (currentUser == null) {
+/////////////////send to login page
+            sendToLoging();
+        }
+
+    }
+
+
+    private void logOut() {
+        mAuth.signOut();
+        sendToLoging();
+    }
+
+
+    private void sendToLoging() {
+
+        Intent intent=new Intent(MainActivity.this,LoginActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
---
+++ app/src/main/java/tech/oyot/SetupActivity.java
@@ -0,0 +1,169 @@
+package tech.oyot;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class SetupActivity extends AppCompatActivity {
+
+
+
+    private CircleImageView setupimage;
+    private Uri resultUri =null;
+
+    private EditText setupName;
+    private EditText setupEmail;
+    private Button setupBtn;
+    private Button setupCancelbtn;
+
+    private StorageReference storageReference;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseFirestore firebaseFirestore;
+//    private android.util.Log Log;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setup);
+
+        Toolbar setupToolbar =findViewById(R.id.setupToolbar);
+        setSupportActionBar(setupToolbar);
+        getSupportActionBar().setTitle("Profile");
+
+        firebaseAuth= FirebaseAuth.getInstance();
+        storageReference= FirebaseStorage.getInstance().getReference();
+        firebaseFirestore=FirebaseFirestore.getInstance();
+
+        setupimage=findViewById(R.id.setup_image);
+        setupName=findViewById(R.id.setup_name);
+        setupEmail=findViewById(R.id.setup_email);
+        setupBtn=findViewById(R.id.setup_btn);
+        setupCancelbtn=findViewById(R.id.setup_cancel_btn);
+
+
+
+        /////////////////////////////
+
+        setupCancelbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mainIntent=new Intent(SetupActivity.this, MainActivity.class);
+                startActivity(mainIntent);
+                finish();
+            }
+        });
+
+//////////////////////////////////
+        setupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+////////// EVERYTHING TO FIREEBASE STORAGE
+                String user_name=setupName.getText().toString();
+                String user_email=setupEmail.getText().toString();
+
+
+                if (!TextUtils.isEmpty(user_name)){  /////check if image is empty;
+
+                    String user_id=firebaseAuth.getCurrentUser().getUid();
+
+//                    StorageReference image_path=storageReference.child("profile_image").child(user_id+ ".jpg");
+//                    image_path.putFile(bitmap).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+//                        @Override
+//                        public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+//
+//                            if(task.isSuccessful()){
+//
+//                            }else {
+//                                String error=task.getException().getMessage();
+//                                Toast.makeText(SetupActivity.this, "error: "+error,Toast.LENGTH_LONG).show();
+//                            }
+//
+//                        }
+//                    });
+
+                    ///////////////////////////////////////////////////////
+                    ///save on firebaseFirestore
+                    ////////schema
+
+                    Map <String, String> userMap= new HashMap<>();
+                    userMap.put("name", user_name);
+                    userMap.put("email", user_email);
+
+                    firebaseFirestore.collection("Users").document(user_id).set(userMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+
+                            if (task.isSuccessful()){
+                                Toast.makeText(SetupActivity.this, "Profile Save ",Toast.LENGTH_LONG).show();
+                                Intent mainIntent= new Intent(SetupActivity.this,MainActivity.class);
+                                startActivity(mainIntent);
+                                finish();
+
+                            }else{
+                                String error=task.getException().getMessage();
+                                Toast.makeText(SetupActivity.this, "error: "+error,Toast.LENGTH_LONG).show();
+                            }
+
+                        }
+                    });
+                    ////////////////////////////////////////////////////////////////////////
+                }
+
+            }
+        });
+
+
+/////////////////////////////////////////////////////////////////////
+        setupimage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+
+                    if (ContextCompat.checkSelfPermission(SetupActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+
+                        Toast.makeText(SetupActivity.this,"Permission denied",Toast.LENGTH_LONG).show();
+                        ActivityCompat.requestPermissions(SetupActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},1);
+
+                    }else{
+                        Toast.makeText(SetupActivity.this,"Pick an image",Toast.LENGTH_LONG).show();
+
+
+
+                    }
+                }
+
+            }
+        });
+
+
+    }
+
+}
