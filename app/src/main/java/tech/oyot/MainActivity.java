81a0cca651e9fe176f559a4bb6915ccf714fca1d
jean catulle <jjcatulle389@gmail.com>
2019-03-14 17:29:27 -0400

    3/14-2

--- app/src/main/java/tech/oyot/MainActivity.java
+++ app/src/main/java/tech/oyot/MainActivity.java
@@ -1,5 +1,5 @@
 package tech.oyot;
-
+//////////i hope it works
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -97,7 +97,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
             }
         });
-        
+
 
     }
 
